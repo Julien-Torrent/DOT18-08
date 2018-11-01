a36a43a0f739576e2ebb92b2c3cb94cb9ceaffe9
Julien Torrent <julien.torrent@outlook.com>
2018-11-01 18:40:06 +0100

    Added Base Website

---
+++ DOT18_08/__init__.py
@@ -0,0 +1,3 @@
+"""
+Package for DOT18_08.
+"""
---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\Projet_TIC\DOT18-08\DOT18-08\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
