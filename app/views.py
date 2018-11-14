1e42cf544eb46374c3939c20ba4fd6d966c5a6f0
Julien Torrent <julien.torrent@outlook.com>
2018-11-14 16:01:52 +0100

    Clean Project

--- app/views.py
+++ app/views.py
@@ -19,28 +19,15 @@ def home(request):
         }
     )
 
-def contact(request):
+def command(request):
     """Renders the contact page."""
     assert isinstance(request, HttpRequest)
     return render(
         request,
-        'app/contact.html',
+        'app/command.html',
         {
-            'title':'Contact',
-            'message':'Your contact page.',
-            'year':datetime.now().year,
-        }
-    )
-
-def about(request):
-    """Renders the about page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/about.html',
-        {
-            'title':'About',
-            'message':'Your application description page.',
+            'title':'Command',
+            'message':'Your command page.',
             'year':datetime.now().year,
         }
     )
