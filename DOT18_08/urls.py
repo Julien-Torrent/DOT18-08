e92222c92effb8eeffc953c0d039b1c9b16a05f3
Julien Torrent <julien.torrent@outlook.com>
2018-12-05 21:35:13 +0100

    Add Get and Set for Speed and Mode

--- DOT18_08/urls.py
+++ DOT18_08/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls import url
 import django.contrib.auth.views
 import app.views
 
+
 # Uncomment the next lines to enable the admin:
 from django.conf.urls import include
 from django.contrib import admin
@@ -17,8 +18,13 @@ urlpatterns = [
     # Pages:
     url(r'^$', app.views.home, name='home'),
     url(r'^command$', app.views.command, name='command'),
+
     # API :
-    url(r'^api/forward$', app.api.forward, name='forward'),
+    url(r'^api/setMode/(?P<mode>\d)', app.api.setMode, name='setMode'),
+    url(r'^api/getMode', app.api.getMode, name='getMode'),
+
+    url(r'^api/setSpeed/(?P<speed>\d{0,4})', app.api.setSpeed, name='setSpeed'),
+    url(r'^api/getSpeed', app.api.getSpeed, name='getSpeed'),
 
     # Admin :
     url(r'^admin/', admin.site.urls),
--- app/api.py
+++ app/api.py
@@ -3,5 +3,24 @@ from django.template import RequestContext
 from django.http import HttpResponse
 from django.http import JsonResponse
 
-def forward(request):
-    return JsonResponse({'function':'forward','parameters':[100] })
+Mode = 0    # Current Mode 0=Manual, 1=FullForward, 2=Automatic
+Speed = 0   # 0 to 1000 0=Stopped, 1000=Full Speed
+
+def setMode(request, mode):
+    global Mode
+    Mode = mode
+    return HttpResponse(status=200)
+
+def getMode(request):
+    global Mode
+    return JsonResponse({'Mode' : Mode})
+
+
+def setSpeed(request, speed):
+    global Speed
+    Speed = speed
+    return HttpResponse(status=200)
+
+def getSpeed(request):
+    global Speed
+    return JsonResponse({'Speed': Speed})
