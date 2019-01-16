35c7bb71881f17180b3981370fbf123322d48565
Julien Torrent <julien.torrent@outlook.com>
2019-01-16 15:11:46 +0100

    Added Speed with turn function

--- app/api.py
+++ app/api.py
@@ -49,13 +49,13 @@ def nextMove(request):
 
 # Turn Left of the requested angle
 def turnLeft(request, angle):
-    commands.append({'function' : "turnLeft", 'angle' : int(angle)})
-    return JsonResponse({'function' : "turnLeft", 'angle' : int(angle)})
+    commands.append({'function' : "turnLeft", 'angle' : int(angle),'speed' : int(Speed)})
+    return JsonResponse({'function' : "turnLeft", 'angle' : int(angle),'speed' : int(Speed)})
 
 # Turn Right of the requested angle
 def turnRight(request, angle):
-    commands.append({'function' : "turnRight", 'angle' : int(angle)})
-    return JsonResponse({'function' : "turnRight", 'angle' : int(angle)})
+    commands.append({'function' : "turnRight", 'angle' : int(angle),'speed' : int(Speed)})
+    return JsonResponse({'function' : "turnRight", 'angle' : int(angle),'speed' : int(Speed)})
 
 # getSpeed and add function name 'forward'
 def forward(request):
